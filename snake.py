9a0d525d5eb55b29627a8705e75f0f329ea5b5d0
Ipsha Gupta <91387751+ipshagupta@users.noreply.github.com>
2022-04-03 01:44:11 +0530

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,57 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_FORWARD = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def move(self):
+        for seg in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(new_x, new_y)
+        self.head.forward(MOVE_FORWARD)
+
+    def move_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def move_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def move_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def move_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def check_wall(self):
+        if self.head.xcor() >= 245 or self.head.xcor() <= -245 or self.head.ycor() >= 245 or self.head.ycor() <= -245:
+            return 1
+
+    def add_segment(self, position):
+        new_segment = Turtle('square')
+        new_segment.color('white')
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def grow(self):
+        self.add_segment(self.segments[-1].position())
